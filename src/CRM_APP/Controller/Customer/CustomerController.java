41ccd63228dfaa22b15f314417e6b5aeb5a0b05d
nphbo1998@gmail.com <nph123312213>
2022-04-16 17:04:50 +0700

    update find customer event

--- src/CRM_APP/Controller/Customer/CustomerController.java
+++ src/CRM_APP/Controller/Customer/CustomerController.java
@@ -4,6 +4,7 @@ import CRM_APP.Database.Const;
 import CRM_APP.Database.Database;
 import CRM_APP.Handler.SceneHandler;
 import CRM_APP.Model.Customer;
+import CRM_APP.Model.Employee;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import java.net.URL;
@@ -11,6 +12,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -32,6 +36,9 @@ public class CustomerController {
     @FXML
     private JFXButton btn_addCustomer;
 
+    @FXML
+    private JFXTextField txt_Find;
+
     ObservableList<Customer> customers;
     SceneHandler sceneHandler;
     Database database ;
@@ -39,7 +46,7 @@ public class CustomerController {
     @FXML
     void initialize() {
         populateList();
-
+        findCustomer();
         btn_addCustomer.setOnAction(e ->{
             sceneHandler = new SceneHandler();
             sceneHandler.slideScene(btn_addCustomer, main_Stack, "X", "/CRM_APP/View/Customer/customerDetail.fxml");
@@ -68,5 +75,35 @@ public class CustomerController {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+    private void findCustomer(){
+        txt_Find.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                sceneHandler = new SceneHandler();
+                database = new Database();
+                ObservableList<Customer> customers = FXCollections.observableArrayList();
+                ResultSet row = null;
+                try {
+                    row = database.filterDataInput(Const.CUSTOMER_TABLE, Const.CUSTOMER_NAME, newValue);
+                    while(row.next()){
+                        customer = new Customer();
+                        customer.setId(row.getString(Const.CUSTOMER_ID));
+                        customer.setCusName(row.getString(Const.CUSTOMER_NAME));
+                        customer.setAddress(row.getString(Const.CUSTOMER_ADDRESS));
+                        customer.setPhone(row.getString(Const.CUSTOMER_PHONE));
+                        customer.setTIN(row.getString(Const.CUSTOMER_TIN));
+                        CustomerCellController.cellStack =main_Stack;
+                        customers.add(customer);
+                    }
+                    lv_customer.setItems(customers);
+                    lv_customer.setCellFactory(CustomerCellController -> new CustomerCellController());
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
